b54c2a19a7ccebc2c2cbec8464125ab6410a96fc
enarsinick <nxdmedia@googlemail.com>
2022-01-11 18:45:08 +0000

    Setup front-end folders and scss

---
+++ client/src/components/Nav.js
@@ -0,0 +1,13 @@
+// Imports
+import logo from "../img/logo.svg";
+
+const Nav = () => {
+    return (
+        <nav>
+            <img src={logo} alt="logo" />
+            <button>Add scores</button>
+        </nav>
+    );
+};
+
+export default Nav;
