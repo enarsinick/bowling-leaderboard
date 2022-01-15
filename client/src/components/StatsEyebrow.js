a31002e98bbb5c4d733ff661a7bbd8df47987d5f
enarsinick <nxdmedia@googlemail.com>
2022-01-15 14:16:29 +0000

    Tablet styling completed

---
+++ client/src/components/StatsEyebrow.js
@@ -0,0 +1,35 @@
+const StatsEyebrow = () => {
+    return (
+        <div className="stats-eyebrow">
+            <div className="stats-eyebrow-info">
+                <p>#</p>
+                <p>Name</p>
+            </div>
+            <div className="stats-eyebrow-scores">
+                <div>
+                    <p>Played</p>
+                </div>
+                <div>
+                    <p>Wins</p>
+                </div>
+                <div>
+                    <p>Top Score</p>
+                </div>
+                <div>
+                    <p>Total</p>
+                </div>
+                <div>
+                    <p>Spares</p>
+                </div>
+                <div>
+                    <p>Strikes</p>
+                </div>
+                <div>
+                    <p>Average</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StatsEyebrow;
--- client/src/components/pages/LeaderboardPage.js
+++ client/src/components/pages/LeaderboardPage.js
@@ -1,11 +1,13 @@
 import PageHeader from "../PageHeader";
 import StatsPanel from "../StatsPanel";
+import StatsEyebrow from "../StatsEyebrow";
 
 const LeaderboardPage = () => {
     return (
         <main>
             <PageHeader eyebrow="Leaderboard" title="Stroud Bowling League" />
             <div className="stats">
+                <StatsEyebrow />
                 <StatsPanel />
                 <StatsPanel />
                 <StatsPanel />
